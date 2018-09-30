c5cc836542f2b60f71339724a9690b537fd88178
Nigel <nigel-wong@hotmail.com>
2018-10-01 00:08:16 +0800

    Added solution for 'Shortest Word' algorithm challenge.

--- JavaScript/kyu8/shortestword.js
+++ JavaScript/kyu8/shortestword.js
@@ -1,1 +1,11 @@
+/* Kata Details:
+    
+Simple, given a string of words, return the length of the shortest word(s).
+String will never be empty and you do not need to account for different data types.
 
+*/
+
+function findShort(s) {
+    const splitS = s.split(" ");
+    return splitS.map(word => word.length).reduce((a, b) => Math.min(a, b));
+}
